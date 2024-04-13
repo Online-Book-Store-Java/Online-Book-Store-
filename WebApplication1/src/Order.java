201272d6152e5799abfd3d7ee5e3251f74d159eb
Misuri M Wijewardhana <misuri.malindika@gmail.com>
2024-04-13 20:32:40 +0530

    Add files via upload

---
+++ WebApplication1/src/Order.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author USER
+ */
+class Order {
+
+    Order(String name, String address, String email, String creditCard, String expiryDate) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
