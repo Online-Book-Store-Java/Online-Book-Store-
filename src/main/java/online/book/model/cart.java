f1a3b952b8cc329e71f0c84d386f908a9bac3b3d
Tharindu Vishwajith Ethpatiyawa <136308882+tharinduVish95@users.noreply.github.com>
2024-04-30 12:08:07 +0530

    Add files via upload

---
+++ src/main/java/online/book/model/cart.java
@@ -0,0 +1,19 @@
+package online.book.model;
+
+public class cart extends product {
+	
+		private int quantity;
+		
+		public cart() {
+			
+		}
+
+		public int getQuantity() {
+			return quantity;
+		}
+
+		public void setQuantity(int quantity) {
+			this.quantity = quantity;
+		}
+	
+}
