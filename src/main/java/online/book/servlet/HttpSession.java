5b66080d3aa31f4ba90051d07328ba183c831209
Misuri M Wijewardhana <misuri.malindika@gmail.com>
2024-04-30 21:59:35 +0530

    Add files via upload

---
+++ src/main/java/online/book/servlet/HttpSession.java
@@ -0,0 +1,12 @@
+package online.book.servlet;
+
+import online.book.model.User;
+
+public class HttpSession {
+
+    public User getAttribute(String string) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getAttribute'");
+    }
+
+}
